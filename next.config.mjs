234fd41c4329886b6623f14496939033b1f3dd25
ashish784 <mauryaashish784@gmail.com>
2024-10-05 03:18:37 +0530

    mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'utfs.io',
-                port: '', // Optional, leave it empty if not needed
-                pathname: '/**', // Allow all paths
-            },
-        ],
+      domains: ['utfs.io'],
     },
-};
-
-export default nextConfig;
+  }
+  
+  export default nextConfig
